6e2a03a46a7d19ce9a1638e0197588e4faf24813
KunAce <wxkace@gmail.com>
2020-07-28 23:12:18 -0700

    Added basic 'user-registrar' component

--- task-engine-web/src/app/app.module.ts
+++ task-engine-web/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
+import { UserRegistrarComponent } from './user-registrar/user-registrar.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserRegistrarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
