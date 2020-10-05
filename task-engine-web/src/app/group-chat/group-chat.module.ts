deb0d1843088ee2afdaa76b2813b352a46106159
KunAce <wxkace@gmail.com>
2020-10-04 20:13:56 -0700

    Added 'GroupChat' module

---
+++ task-engine-web/src/app/group-chat/group-chat.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GroupChatComponent } from './group-chat.component';
+
+@NgModule({
+  declarations: [GroupChatComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class GroupChatModule { }
